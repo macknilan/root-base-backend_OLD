c9056474176dc8567f480acc4bed6840bd5233f6
Rodolfo Ugalde 8a <rodolfougaldeochoa@gmail.com>
2021-07-16 23:33:02 -0500

    :sparkles: [ADD] For the User add SignUp Login Verify and UpDate
    Se agrego para Usuario el Login, SignUp, Login y Verify el mensaje de que sen envia en la consola para verificar el token, faltan hacer proebas para verificar que funcionen correctamente, ESTE ES DEL COMMIT ANTERIOR

--- config/settings/local.py
+++ config/settings/local.py
@@ -59,7 +59,7 @@ if env("USE_DOCKER") == "yes":
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ["django_extensions"]  # noqa F405
 
-# Your stuff...
+# My stuff...
 # ------------------------------------------------------------------------------
 
 # drf spectacular
